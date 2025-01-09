2237620415e3a6a08a8aac4cfa5dd2d9bb4d5908
Vladyslav <vladnazarchuk406@gmail.com>
2025-01-09 20:30:02 +0100

    firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,9 @@
-// Insert Firebase configuration here
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// Insert Firebase configuration here i Odkomentuj cały kod.
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app); 
+
+// export { db }; firebaseConfig.js
